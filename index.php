240bf8b8c6eb3e6b9aa2cfef6f4f38c896f2bdfd
morik196911 <morik1969-morik1969@yandex.ru>
2024-12-06 20:42:33 +0400

    first programm, connection,async, defer

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="./public/css/bootstrap.css">
+</head>
+<body>
+<script async src="public/myscript/main.js"></script>
+
+  <h2 class = "text-success">Total one</h2>
+  <script>
+    document.write("The first programm Javascript")
+  </script>
+   <h2 class = "text-secondary">Total two</h2>
+  <script>
+    document.write("The second programm Javascript")
+    const sum = 2 + 5;
+    document.write("<br/>"+sum)
+    document.write("<br/>");
+    document.write("Hello");
+    {
+      document.write("<br/>How about you");
+      document.write("<br/>Are you sleeping ?");
+    }
+    document.write("<br/> By");
+  </script>
+
+
+  <script src="./public/js/bootstrap.js"></script>
+</body>
+</html>
