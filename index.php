1be43c8d10410e9105f33bd5b6b63c509325d758
morik196911 <morik1969-morik1969@yandex.ru>
2024-12-08 21:00:10 +0400

    function_lesson_1

--- index.php
+++ index.php
@@ -9,45 +9,96 @@
 <body>
 
   <script>
-const people = ["user","Hena"];
-const foo = Array(3);
-foo[0] = "f0";
-foo[1] = "f1";
-//foo[2] = "f2";
-//foo[3] = "f3";
-console.log(foo)
-people[5] = "Tom";
-console.log(people)
-const number = [[1,2,3],[5,6,7]]
- const people2= [
-        ["Tom", 25, false],
-        ["Bill", 38, true],
-        ["Alice", 21, false]
-]
-console.log(people2[1][0])
-let foo1 = 5;
-switch (foo1) {
-  case 5:
-    console.log("5 ok")
-    foo1 += 25
-    //break;
-    case 4:
-    console.log("4 ok", foo1)
-    break;
-    case 3:
-    console.log("3 ok")
-    //break;
-
-  default:
-    console.log("In not number")
-    break;
+    let i = null;
+function hello(name){
+  ++i;
+  console.log("Hello my dear "+name)
 }
-for(let i = 0; i < people.length ; i++){
-  document.write(people[i]+"<br/>")
+hello("Alex")
+hello("Tom")
+hello("Lena")
+hello("Masha")
+document.write("Result i "+i+"<br/>")
+const foo = (name,age)=>{
+  return(`My name is ${name} and  my age ${age}`)
 }
-for(let i = 0; i < 10 ; i +=2){
-  console.log(`Result ${i} == `,++i)
+function hello2(){
+  return("Hy hello2")
 }
+function goodMorning(){
+  return("Good Morning")
+}
+function goodEvening(){
+  return("Good Evening")
+}
+let message = hello2
+document.write(foo("Kola",86))
+document.write("<br/>")
+document.write(message()+"<br/>")
+ message = goodEvening;
+ document.write(message()+"<br/>")
+ document.write("###################################<br/>")
+ function print(number){
+  //let n = number;
+  local_print()
+  local_print()
+  local_print()
+  function local_print(){
+    ++number;
+    document.write("Hello local = "+ number+"<br/>")
+  }
+ }
+ print(5)
+ document.write("###################################<br/>")
+ function respect(name = "Feda",age = 25){
+  return(`This name ${name} and age ${age}`)
+ }
+ document.write("Result param "+ respect("Sada",84))
+ document.write("###################################<br/>")
+ const tom = ["Tom",25,"yandex"];
+ const bob = ["Bob", 36, "mail"];
+ function printPerson(name,age,email){
+  return(`This name ${name} and ${age} and ${email}`)
+ }
+ document.write(printPerson(...tom)+"<br/>")
+ /* Функции с произвольным количеством параметров*/
+ function sum(){
+  let result = 0;
+  for(let item of arguments){
+    result += item;
+  }
+  document.write(result+"<br/>");
+ }
+ sum();
+ sum(25,80,96);
+ sum(5,9);
+ function sum2(x,y,t){
+  let result = 0;
+  for(let item of arguments){
+    result += item;
+  }
+  document.write("Result sum2 "+result+"<br/>");
+  console.log("Arguments lenght ", arguments.length)
+ }
+ sum2(2,4,6);
+ sum2(25,25,96,84,5)
+ /* С помощью оператора ... (rest-оператор) также можно передать переменное количество значений:*/
+ function sum3(param, ...numbers){
+  let result = 0;
+  for(let item of numbers){
+    result += item;
+  }
+  document.write("Result sum333 "+result+"<br/>");
+  console.log("Numbers lenght ", numbers.length , param)
+ }
+ sum3("Dima", 5,8,9,6)
+ /* Функции в качестве параметров*/
+ function summma(x , y){return x + y}
+ function minus(x,y){return x - y};
+ const operation = (x,y,func)=>{
+  document.write("Result sum 44 "+func(x,y)+"<br/>")
+ }
+ operation(2,4,summma);
   </script>
 
 
