e83a10b16b0aac4ee6a55a6c9f4c5b19717261a3
morik196911 <morik1969-morik1969@yandex.ru>
2024-12-09 20:07:48 +0400

    function_lesson_3

--- index.php
+++ index.php
@@ -8,41 +8,29 @@
 </head>
 <body>
 
-  <script>
-let user , t, foo = ()=>{return 258} ;
-user = "Toni";
-const name = "Gena";
-console.log(name)
-const arr = ["Boolean","String","BigInt","Undefined",
-"Symbol","Null","Number","Object"]
-
-document.write("<ul>");
-for(let item of arr){
-  document.write("<li class = 'text-primary'>"+item + "</li>");
+<script>
+/* Переопределение функций */
+function print(){
+  console.log("Hello morning");
+  print = function(){
+    console.log("Good evening");
+  }
 }
-document.write("</ul>");
-document.write("<br/>"+Number.MIN_VALUE)
-document.write("<br/>"+Number.POSITIVE_INFINITY)
-document.write("<br/> type param "+typeof arr)
-let ff = `My name ${name}`
-document.write("<br/>"+ff);
-const text = `Hello
-My family
-Mother
-Fathre
-`
-document.write(text)
-console.log(text)
-let data , email = null;
-document.write("<br/>"+data + " == "+email)
-const User = {};
-User.name = "Lena";
-User.age = 25;
-document.write("<br/> Object typeof "+typeof User)
-console.log(User)
-document.write("<br/> Object typeof "+Object.entries(User))
-let id ;
-document.write("<br/>  typeof "+typeof id)
+print();
+print();
+print();
+console.log("@@@@@@@@@@@@@@@@@@@@@@@@@@")
+const qq = print;
+qq()
+qq()
+/* Передача параметров по значению и по ссылке */
+function user(user){
+ user.name = "Tom"
+}
+let bob = {fname:"Smirnoff",age:25}
+user(bob);
+console.log(bob.name)
+
   </script>
 
 
